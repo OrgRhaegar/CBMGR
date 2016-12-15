0eb86a055b70d32b5cb517b9515aac4f026fb2d9
Rhaegar1204 <rhaegar1204@163.com>
2016-12-14 22:54:53 -0800

    Add log operations.

---
+++ Application/CBMG.Common/Entity/Log.cs
@@ -0,0 +1,90 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Log.cs" company="RGS">
+//     Copyright RGS. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CBMGR.Common
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public class Log
+    {
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the Log class.
+        /// </summary>
+        public Log()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Log class.
+        /// </summary>
+        /// <param name="msg">Log message</param>
+        public Log(string msg)
+        {
+            this.LogMessage = msg;
+            this.Excep = null;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Log class.
+        /// </summary>
+        /// <param name="ex">Log exception</param>
+        public Log(Exception ex)
+        {
+            this.LogMessage = string.Empty;
+            this.Excep = ex;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Log class.
+        /// </summary>
+        /// <param name="msg">Log message</param>
+        /// <param name="ex">Log exception</param>
+        public Log(string msg, Exception ex)
+        {
+            this.LogMessage = msg;
+            this.Excep = ex;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets log message.
+        /// </summary>
+        public string LogMessage { get; set; }
+
+        /// <summary>
+        /// Gets or sets log exception.
+        /// </summary>
+        public Exception Excep { get; set; }
+        #endregion
+
+        #region Pulbic methods
+        /// <summary>
+        /// Write log
+        /// </summary>
+        public void Write()
+        {
+            string logFiel = GlobalConfig.GlobalPars["LogFile"];
+            using (FileStream fs = new FileStream(logFiel, FileMode.OpenOrCreate))
+            {
+                StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+                sw.WriteLine("Date:{0}", DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"));
+                sw.WriteLine("Message:{0}", this.LogMessage);
+                if (this.Excep != null)
+                {
+                    sw.WriteLine(this.Excep);
+                }
+
+                sw.Close();
+                fs.Close();
+            }
+        }
+        #endregion
+    }
+}
---
+++ Application/CBMG.Common/LogQueue.cs
@@ -0,0 +1,77 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="LogQueue.cs" company="RGS">
+//     Copyright RGS. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CBMGR.Common
+{
+    using System.Collections.Generic;
+    using System.Threading;
+
+    public class LogQueue
+    {
+        #region Fields
+        /// <summary>
+        /// Queue of logs.
+        /// </summary>
+        private static Queue<Log> logs;
+
+        /// <summary>
+        /// Thread to write logs.
+        /// </summary>
+        private static Thread logThread;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Initializes static members of the LogQueue class.
+        /// </summary>
+        static LogQueue()
+        {
+            logs = new Queue<Log>();
+            logThread = new Thread(CheckLogQueue);
+            logThread.IsBackground = true;
+            logThread.Start();
+        }
+        #endregion
+
+        #region Public methods
+        /// <summary>
+        /// Add a log to queue
+        /// </summary>
+        /// <param name="log">Log to add</param>
+        public void AddToLogQueue(Log log)
+        {
+            lock (logs)
+            {
+                logs.Enqueue(log);
+            }
+        }
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// Check the queue if there is any log to write.
+        /// </summary>
+        private static void CheckLogQueue()
+        {
+            while (true)
+            {
+                lock (logs)
+                {
+                    if (logs.Count > 0)
+                    {
+                        Log log = logs.Dequeue();
+                        log.Write();
+                    }
+                    else
+                    {
+                        Thread.Sleep(3000);
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
