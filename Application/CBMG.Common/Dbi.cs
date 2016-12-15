c3585cf023a7ceb045ba98fac6b9e3dbaee08ece
Rhaegar1204 <rhaegar1204@163.com>
2016-12-14 21:59:22 -0800

    update db operations

--- Application/CBMG.Common/Dbi.cs
+++ Application/CBMG.Common/Dbi.cs
@@ -14,37 +14,12 @@ namespace CBMGR.Common
     /// </summary>
     public class Dbi
     {
-        #region Fields
-        /// <summary>
-        /// Database connection
-        /// </summary>
-        private SqlConnection con;
-        #endregion
-
         #region Constructor
         /// <summary>
         /// Initializes a new instance of the Dbi class.
         /// </summary>
         public Dbi()
         {
-        }
-        #endregion
-
-        #region Properties
-        /// <summary>
-        /// Gets database connection.
-        /// </summary>
-        private SqlConnection Connection
-        {
-            get
-            {
-                if (this.con == null)
-                {
-                    this.RestConnection();
-                }
-
-                return this.con;
-            }
         }
         #endregion
 
@@ -56,48 +31,63 @@ namespace CBMGR.Common
         public string TestConnection()
         {
             string testResult = string.Empty;
-            try
+            using (SqlConnection con = this.CreateConnection())
             {
-                SqlCommand command = this.Connection.CreateCommand();
-                command.CommandText = "SELECT TOP 10 NAME FROM SYSOBJECTS";
-                this.Connection.Open();
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                testResult = ex.Message;
-            }
-            finally
-            {
-                this.Connection.Close();
+                try
+                {
+                    SqlCommand command = con.CreateCommand();
+                    command.CommandText = "SELECT TOP 10 NAME FROM SYSOBJECTS";
+                    con.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    testResult = ex.Message;
+                }
             }
 
             return testResult;
         }
 
-        /// <summary>
-        /// Resutl SQL connecion.
-        /// </summary>
-        /// <returns>Reset result</returns>
-        public bool RestConnection()
+        public int ExecuteNoneQuery(string sql, SqlParameter parameter)
         {
-            bool resetResult = true;
-            try
+            int queryResult = -1;
+            using (SqlConnection con = this.CreateConnection())
             {
-                string conStr = "Data Source={0};Initial Catalog={1};User ID={2};Password={3}";
-                string dbServer = GlobalConfig.GlobalPars["DBServer"];
-                string dbName = GlobalConfig.GlobalPars["DBName"];
-                string dbUser = GlobalConfig.GlobalPars["DBUser"];
-                string dbPwd = GlobalConfig.GlobalPars["DBPassword"];
-                conStr = string.Format(conStr, dbServer, dbName, dbUser, dbPwd);
-                this.con = new SqlConnection(conStr);
-            }
-            catch (Exception ex)
-            {
-                resetResult = false;
+                try
+                {
+                    SqlCommand command = con.CreateCommand();
+                    command.CommandText = sql;
+                    command.Parameters.Add(parameter);
+                    con.Open();
+                    queryResult = command.ExecuteNonQuery();
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+                    // Tudo: add to log querys
+                }
             }
 
-            return resetResult;
+            return queryResult;
+        }
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// Create a instance of sql connection.
+        /// </summary>
+        /// <returns>New connection</returns>
+        private SqlConnection CreateConnection()
+        {
+            string conStr = "Data Source={0};Initial Catalog={1};User ID={2};Password={3}";
+            string dbServer = GlobalConfig.GlobalPars["DBServer"];
+            string dbName = GlobalConfig.GlobalPars["DBName"];
+            string dbUser = GlobalConfig.GlobalPars["DBUser"];
+            string dbPwd = GlobalConfig.GlobalPars["DBPassword"];
+            conStr = string.Format(conStr, dbServer, dbName, dbUser, dbPwd);
+            SqlConnection con = new SqlConnection(conStr);
+            return con;
         }
         #endregion
     }
