478bea7e103305828d26736a6cac88d770010764
Rhaegar1204 <rhaegar1204@163.com>
2016-12-14 20:37:59 -0800

    Add other files.

---
+++ Application/CBMG.Common/Dbi.cs
@@ -0,0 +1,104 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Dbi.cs" company="RGS">
+//     Copyright RGS. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CBMGR.Common
+{
+    using System;
+    using System.Data.SqlClient;
+
+    /// <summary>
+    /// Database operator
+    /// </summary>
+    public class Dbi
+    {
+        #region Fields
+        /// <summary>
+        /// Database connection
+        /// </summary>
+        private SqlConnection con;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the Dbi class.
+        /// </summary>
+        public Dbi()
+        {
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets database connection.
+        /// </summary>
+        private SqlConnection Connection
+        {
+            get
+            {
+                if (this.con == null)
+                {
+                    this.RestConnection();
+                }
+
+                return this.con;
+            }
+        }
+        #endregion
+
+        #region Pulbic methods
+        /// <summary>
+        /// Test SQL connecion.
+        /// </summary>
+        /// <returns>Test result. Empty for good.</returns>
+        public string TestConnection()
+        {
+            string testResult = string.Empty;
+            try
+            {
+                SqlCommand command = this.Connection.CreateCommand();
+                command.CommandText = "SELECT TOP 10 NAME FROM SYSOBJECTS";
+                this.Connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                testResult = ex.Message;
+            }
+            finally
+            {
+                this.Connection.Close();
+            }
+
+            return testResult;
+        }
+
+        /// <summary>
+        /// Resutl SQL connecion.
+        /// </summary>
+        /// <returns>Reset result</returns>
+        public bool RestConnection()
+        {
+            bool resetResult = true;
+            try
+            {
+                string conStr = "Data Source={0};Initial Catalog={1};User ID={2};Password={3}";
+                string dbServer = GlobalConfig.GlobalPars["DBServer"];
+                string dbName = GlobalConfig.GlobalPars["DBName"];
+                string dbUser = GlobalConfig.GlobalPars["DBUser"];
+                string dbPwd = GlobalConfig.GlobalPars["DBPassword"];
+                conStr = string.Format(conStr, dbServer, dbName, dbUser, dbPwd);
+                this.con = new SqlConnection(conStr);
+            }
+            catch (Exception ex)
+            {
+                resetResult = false;
+            }
+
+            return resetResult;
+        }
+        #endregion
+    }
+}
---
+++ Application/CBMG.Common/GlobalConfig.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GlobalConfig.cs" company="RGS">
+//     Copyright RGS. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CBMGR.Common
+{
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.Configuration;
+
+    /// <summary>
+    /// Global config
+    /// </summary>
+    public class GlobalConfig
+    {
+        #region Fileds
+        /// <summary>
+        /// Dictionary of global settings.
+        /// </summary>
+        public static readonly Dictionary<string, string> GlobalPars;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Initializes static members of the GlobalConfig class.
+        /// </summary>
+        static GlobalConfig()
+        {
+            GlobalPars = new Dictionary<string, string>();
+            NameValueCollection settings = ConfigurationManager.AppSettings;
+            foreach (string key in settings.AllKeys)
+            {
+                GlobalPars.Add(key, settings[key]);
+            }
+        }
+        #endregion
+    }
+}
