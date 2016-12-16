7126a54bcc4a46b898e1be008df939384d91c280
Rhaegar1204 <rhaegar1204@163.com>
2016-12-15 21:27:37 -0800

    Add projcet of interface.

---
+++ Application/CBMGR.Ingerface/IUser.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IUser.cs" company="RGS">
+//     Copyright RGS. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CBMGR.Ingerface
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Interface of user
+    /// </summary>
+    public interface IUser
+    {
+    }
+}
