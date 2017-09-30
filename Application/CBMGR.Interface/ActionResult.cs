51d97adf64fb445532db67f921516682b4e00143
Rhaegar1204 <rhaegar1204@163.com>
2017-09-30 15:15:42 +0800

    Add action result.

---
+++ Application/CBMGR.Interface/ActionResult.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ActionResult.cs" company="RGS">
+//     Copyright RGS. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CBMGR.Interface
+{
+    using System;
+
+    /// <summary>
+    /// Common action result of method.
+    /// </summary>
+    public class ActionResult
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether calling of method is success.
+        /// </summary>
+        public bool Result { get; set; }
+
+        /// <summary>
+        /// Gets or sets result value of method calling, can be any data type.
+        /// </summary>
+        public object ResultValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets addtional message of method calling.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets exception of method calling.
+        /// </summary>
+        public Exception Ex { get; set; }
+    }
+}
