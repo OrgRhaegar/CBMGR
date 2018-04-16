3f8e32f13fdf0bf43a4182467a186cb8724edbbf
Rehagar <rhaegar1204@163.com>
2018-04-16 00:01:43 -0700

    Add authorization.

---
+++ Application/CBMGR.WebApi/Controllers/AuthAttribute.cs
@@ -0,0 +1,24 @@
+﻿namespace CBMGR.WebApi.Controllers
+{
+    #region using
+    using System.Net.Http.Headers;
+    using System.Web.Http;
+    using System.Web.Http.Controllers;
+    #endregion
+
+    public class AuthAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            AuthenticationHeaderValue auth = actionContext.Request.Headers.Authorization;
+            if (auth != null && auth.Parameter != null)
+            {
+                base.OnAuthorization(actionContext);
+            }
+            else
+            {
+                base.HandleUnauthorizedRequest(actionContext);
+            }
+        }
+    }
+}
---
+++ Application/CBMGR.WebApi/Controllers/CommomController.cs
@@ -0,0 +1,15 @@
+﻿namespace CBMGR.WebApi.Controllers
+{
+    #region using
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    #endregion
+
+    public class CommomController : ApiController
+    {
+    }
+}
